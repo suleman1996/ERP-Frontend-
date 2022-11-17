8b54f2c5edbbef30c8961c81c1d6f92c78ac1881
waqarhussain2 <waqar.hussain@sprintx.net>
2022-11-17 11:48:42 +0500

    issue calender

--- src/pages/calender/index.tsx
+++ src/pages/calender/index.tsx
@@ -158,7 +158,7 @@ const Calender = () => {
       venue: venue ? venue : '',
       category: category ? { label: category, value: category } : '',
       description,
-      allDay: allDay,
+      allDay: allDay ? allDay : '',
       attendees: attendees
         ? attendees?.map(({ _id, fullName }: any) => {
             return { label: fullName, value: _id };
@@ -314,7 +314,7 @@ const Calender = () => {
                 setOpenModal(true);
                 setSingleEventData('');
                 setSelectedFileNameBack('');
-                setCheck(false);
+                setCheck(!check);
               }}
               iconStart={plus}
             />
