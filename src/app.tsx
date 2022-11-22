2d8c0b9659ca3a40f325d5843aa1e51f7241ae36
Hamza Ali Khalid <hamzaig@yahoo.com>
2022-11-22 11:33:46 +0500

    testing eslint

--- src/app.tsx
+++ src/app.tsx
@@ -13,7 +13,7 @@ import NotificationService from 'services/notification-service'
 
 import 'bootstrap-daterangepicker/daterangepicker.css'
 import { getAllSettings } from 'store/actions'
-
+//only for commit
 const App = () => {
   const dispatch = useAppDispatch()
   const { user_id, currentUser, token } = useAppSelector((state) => state?.app)
