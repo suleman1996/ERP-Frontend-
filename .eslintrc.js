9c7bc7eecfcb10f6b4f6398f82dabc012f879267
Hamza Ali Khalid <hamzaig@yahoo.com>
2022-11-22 12:07:21 +0500

    abc

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,10 @@ module.exports = {
     es2021: true,
   },
   extends: 'plugin:react/recommended',
+  overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
+    ecmaVersion: 'latest',
     sourceType: 'module',
   },
   plugins: ['react', '@typescript-eslint'],
